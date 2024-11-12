0e77fc4f93e50b6986f190ab11b554115f9e7402
hazrat120 <125098771+hazrat120@users.noreply.github.com>
2024-11-12 21:54:04 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Test</title>
+</head>
+<body>
+    <h1 style="text-aligh:center;">Hello PHP</h1>
+<?php 
+// Note: However; all variable names are case-sensitive!
+    echo "Hello World! <br>";
+    ECHO "Hello World! <br>";
+    Echo "Hello World! <br>";
+
+    $color = "red";
+    echo "My car is " . $color . "<br>";
+/*     echo "My house is " . $COLOR . "<br>";
+    echo "My boat is " > $coLOR . "<br>"; */
+    
+    $x = 5  /* +15 */ + 5;
+    echo $x;
+?>
+
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,1 @@
+# In PHP, keywords (e.g. if, else, while, echo, etc.), classes, functions, and user-defined functions are not case-sensitive.
